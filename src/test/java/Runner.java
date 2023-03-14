3a6c515bb50dc23466a8431670c8628dccd2c439
Mariana Pulido <109924753+lpulidoa@users.noreply.github.com>
2023-03-14 10:13:30 -0500

    Add files via upload

---
+++ src/test/java/Runner.java
@@ -0,0 +1,21 @@
+import org.testng.annotations.Test;
+import pages.ArticlePage;
+import pages.LandingPage;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+public class Runner extends Hooks{
+
+    @Test
+    public void searchTest(){
+        LandingPage landingPage = new LandingPage(driver);
+        landingPage.searchQuery("Endava")
+                .waitForSearchResults();
+
+        ArticlePage articlePage = landingPage.clickFirstResult();
+
+        assertThat("The title was not correct",
+                articlePage.getTitle(), equalTo("Endava"));
+    }
+
+}
